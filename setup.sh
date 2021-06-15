b9e559d021e7329b158ee7b4c717df468b999375
Mr.programmer <78996423+mrprogrammer2938@users.noreply.github.com>
2021-06-14 22:27:31 -0400

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# This code write by Mr.nope
+clear
+printf '\033]SM-See/Installing'
+echo "Installing..."
+sleep 2
+chmod a+x SM-See
+sudo cp SM-See /usr/bin
+echo ""
+echo "Installing..., Finish...!"
+echo ""
+exit 1
